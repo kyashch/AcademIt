472d553ac0fa3e4a0b2564b4d6155e5f8916fbb3
yashch <k.yashch>
2020-01-10 23:52:15 +0700

    ArrayListHome Исправление ошибок.

--- ArrayListHome/src/ru/academits/yashch/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/yashch/arraylisthome/ArrayListHome.java
@@ -7,38 +7,40 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> list1 = new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<String> recordList = new ArrayList<>();
 
         try (Scanner scanner = new Scanner(new FileInputStream("input.txt"))) {
             while (scanner.hasNextLine()) {
-                list1.add(scanner.nextLine());
+                recordList.add(scanner.nextLine());
+            }
+
+            System.out.println(recordList);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        }
+
+        ArrayList<Integer> integersListWithoutEvenNumbers = new ArrayList<>(Arrays.asList(4, 0, 8, 1, 5, 3, 1, 8, 3, 4, 5));
+
+        for (int i = 0; i < integersListWithoutEvenNumbers.size(); ++i) {
+            if (integersListWithoutEvenNumbers.get(i) % 2 == 0) {
+                integersListWithoutEvenNumbers.remove(i);
+
+                --i;
             }
         }
 
-        System.out.println(list1);
+        System.out.println(integersListWithoutEvenNumbers);
 
-        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 6, 7, 2, 5, 3, 1, 8, 3, 4, 5));
+        ArrayList<Integer> integersListWithRepetitions = new ArrayList<>(Arrays.asList(9, 1, 3, 6, 8, 1, 6, 3, 7, 8, 9));
+        ArrayList<Integer> integersListWithoutRepetitions = new ArrayList<>();
 
-        for (int i = 0; i < list2.size(); ++i) {
-            if (list2.get(i) % 2 == 0) {
-                list2.remove(i);
+        for (Integer e : integersListWithRepetitions) {
+            if (!integersListWithoutRepetitions.contains(e)) {
+                integersListWithoutRepetitions.add(e);
             }
         }
 
-        System.out.println(list2);
-
-        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(9, 1, 3, 6, 8, 1, 6, 3, 7, 8, 9));
-        ArrayList<Integer> list4 = new ArrayList<>();
-
-        for (int e : list3) {
-            if (list4.contains(e)) {
-                continue;
-            }
-
-            list4.add(e);
-        }
-
-        System.out.println(list4);
+        System.out.println(integersListWithoutRepetitions);
     }
 }
